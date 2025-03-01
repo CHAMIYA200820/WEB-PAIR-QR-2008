76cf6235e5fcdb016305a0e143afec51ae912ada
CHAMIYA200820 <chamindumax33333@gmail.com>
2025-03-01 07:28:38 +0530

    Update id.js

--- id.js
+++ id.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = " *PINk QUEEN MD*_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let characters = " *PINk QUEEN MD-ABCDEFabcdersvwxyz0156789";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
